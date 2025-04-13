383841d3634f64ea55bfad5532bbf0a4371230b5
douglas-i <hdouglasu@gmail.com>
2025-04-12 23:43:43 -0600

    Add ServiceRegistration

---
+++ DET.Common/Model/ServiceDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DET.Common.Model
+{
+    public class ServiceDefinition
+    {
+        public string Interface { get; set; } = string.Empty;
+        public string Implementation { get; set; } = string.Empty;
+        public string Lifetime { get; set; } = "Scoped";
+    }
+}
